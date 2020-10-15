fc9f34e1ba3cdde42174f514873292d30d8beacd
Ahmed Ruhul Quddos Joyon <ahjoyo@ttu.ee>
2020-10-15 18:24:47 +0300

    HW2

---
+++ HW2/HomeWork2/HomeWork2/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Xml.Schema;
+
+namespace HomeWork2
+{
+    class Program
+    {
+        
+        
+        public static void Main(string[] args)
+        {
+            Console.WriteLine(" Hello Bob and Alice!!!");
+            Console.WriteLine("Please enter common modulus p of a prime number");
+            var q = Console.ReadLine();
+            var p = Convert.ToUInt64(q);
+            Validator validator = new Validator();
+            Calculation calculation=new Calculation();
+            while (!validator.IsPrime(p))
+            {
+                Console.WriteLine("Please give me a valid number");
+                break;
+            }
+            Console.WriteLine($"your modulus number is {p}");
+            Console.WriteLine($"Now, give me base g of a primitive root modulo {p}:");
+            var s = Console.ReadLine();
+            var r = Convert.ToUInt64(s);
+           
+
+            while(!validator.IsPrimitiveRoot(r,p))
+            {
+                Console.WriteLine("please give me a valid number");
+                break;
+            }
+            
+            Console.WriteLine(" Hello Alice!!!");
+            Console.WriteLine($"Give me a secret integer between(0,{p})");
+            var g = Console.ReadLine();
+            var G = Convert.ToUInt64(s);
+            while (!calculation.IsInRange(G, p))
+            {
+                Console.WriteLine("Out of Range");
+                Console.WriteLine("plese provide a valid number");
+                break;
+            }
+            Console.WriteLine($"Alices secret number is {G}");
+            ulong compA = calculation.PowWithMod(r, G, p);
+            Console.WriteLine($"Alice's computed number is {compA}");
+            
+            Console.WriteLine(" Hello Bob!!!");
+            Console.WriteLine($"Give me a secret integer between(0,{p})");
+            var S = Console.ReadLine();
+            var R = Convert.ToUInt64(s);
+            while (!calculation.IsInRange(R, p))
+            {
+                Console.WriteLine("Out of Range");
+                Console.WriteLine("plese provide a valid number");
+                break;
+            }
+            Console.WriteLine($"Bobs secret number is {R}");
+            ulong compB = calculation.PowWithMod(r, R, p);
+            Console.WriteLine($"Bob's computed number is {compB}");
+            
+            Console.WriteLine($"Alice , bob sent you {compB}");
+            ulong secretAlice = calculation.PowWithMod(compB, G, p);
+            
+            Console.WriteLine($"Bob , Alice sent you {compA}");
+            ulong secretBob = calculation.PowWithMod(compA, G, p);
+            
+            Console.WriteLine($"Alice has {secretAlice} and Bob has {secretBob}");
+            
+
+
+
+          
+
+
+
+
+
+
+        }
+    }
+}
---
+++ HW2/HomeWork2/HomeWork2/Validator.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace HomeWork2
+{
+    public  class Validator
+    {
+
+        public static Boolean IsInt(string a)
+        {
+            int result;
+            bool parsedSuccessfully = int.TryParse(a, out result);
+            if (parsedSuccessfully == false)
+            {
+                Console.WriteLine("Please Provide an integer");
+                return false;
+            }
+
+            return true;
+        }
+
+        public  Boolean IsPrime(ulong n)
+        {
+            string v = n.ToString();
+            int m, flag = 0;
+            m = (int) (n / 2);
+            if (IsInt(v))
+            {
+                for (int i = 2; i <= m; i++)
+                {
+                    if (n % (ulong) i == 0)
+                    {
+                        Console.WriteLine("It is not a prime");
+                        flag = 1;
+                        break;
+                    }
+
+                }
+
+                if (flag == 0)
+                {
+                    return true;
+                }
+
+                return false;
+
+            }
+
+            return false;
+
+        }
+
+        public  bool IsPrimitiveRoot(ulong r, ulong p)
+        {
+            for (ulong i = 1; i < p; i++)
+            {
+                if (i * Math.Log(r) > Math.Log(ulong.MaxValue))
+                    throw new System.ArgumentException("Overflow Error.");
+                if ((ulong) Math.Pow(r, i) % p == 1)
+                {
+                    if (i == p - 1)
+                        return true;
+                    Console.WriteLine("Not a primitive root, try again.");
+                    return false;
+                }
+            }
+
+            Console.WriteLine("Not a primitive root, try again.");
+            return false;
+
+
+
+        }
+        
+    }
+}
